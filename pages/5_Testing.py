dadfb26e00324ff32dc13b721fa2d1a745102704
janivana23 <janiceivana@gmail.com>
2025-08-30 00:07:48 +1000

    testing connection

---
+++ pages/5_Testing.py
@@ -0,0 +1,12 @@
+import mysql.connector
+import streamlit as st
+
+conn = mysql.connector.connect(
+    host=st.secrets["mysql"]["host"],
+    user=st.secrets["mysql"]["user"],
+    password=st.secrets["mysql"]["password"],
+    database=st.secrets["mysql"]["database"],
+    port=st.secrets["mysql"]["port"]
+)
+
+st.success("✅ Connected successfully with secrets.toml")
