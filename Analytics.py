2c5667f6ab02416e68e18381c9e39cb6832598e8
janivana23 <janiceivana@gmail.com>
2025-12-18 17:21:53 +1100

    add date month to query

--- Analytics.py
+++ Analytics.py
@@ -85,7 +85,7 @@ def app():
 
 
     # -------------------- Load aggregated station data --------------------
-    query2 = "SELECT train_code, train_name, SUM(train_volume_tap_in) AS total_volume_in, SUM(train_volume_tap_out) AS total_volume_out FROM TRAIN NATURAL JOIN TRAIN_VOLUME GROUP BY train_code, train_name ORDER BY total_volume_in DESC, total_volume_out DESC;"
+    query2 = "SELECT train_code, train_name, SUM(train_volume_tap_in) AS total_volume_in, SUM(train_volume_tap_out) AS total_volume_out, train_volume_year_month FROM TRAIN NATURAL JOIN TRAIN_VOLUME GROUP BY train_code, train_name ORDER BY total_volume_in DESC, total_volume_out DESC;"
 
     listdtype = [
         ("total_volume_in", "int"),
